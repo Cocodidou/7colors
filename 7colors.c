318b54f623b424b4f5f9c78f899974c12b292678
Corentin Ferry <cocodidouf@gmail.com>
2016-02-12 15:33:16 +0100

    split main

--- 7colors.c
+++ 7colors.c
@@ -75,20 +75,3 @@ void update_board(char* board, char player, char color) {
 }
 
 
-/** Program entry point */
-int main()
-{
-  //RNG init
-  srand(time(0));
-   char board[BOARD_SIZE * BOARD_SIZE];
-//    = { 'A' };
-
-
-   printf("\n\n  Welcome to the 7 wonders of the world of the 7 colors\n"
-	      "  *****************************************************\n\n"
-	 "Current board state:\n");
-   fill_board(board);
-   print_board(&(board[0]));
-
-   return 0; // Everything went well
-}
---
+++ main.c
@@ -0,0 +1,20 @@
+#include "7colors.h"
+#include <stdio.h>
+
+/** Program entry point */
+int main()
+{
+  //RNG init
+  srand(time(0));
+   char board[BOARD_SIZE * BOARD_SIZE];
+//    = { 'A' };
+
+
+   printf("\n\n  Welcome to the 7 wonders of the world of the 7 colors\n"
+	      "  *****************************************************\n\n"
+	 "Current board state:\n");
+   fill_board(board);
+   print_board(&(board[0]));
+
+   return 0; // Everything went well
+}
